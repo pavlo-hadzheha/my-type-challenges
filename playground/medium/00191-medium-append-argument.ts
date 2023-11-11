4a0343ee07de207e0ced263b97ae8cc5bafbf1c3
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-12 01:00:16 +0200

    [softonix-assessment] [DONE] 00191-medium-append-argument.ts

--- playground/medium/00191-medium-append-argument.ts
+++ playground/medium/00191-medium-append-argument.ts
@@ -5,7 +5,10 @@
 
   ### Question
 
-  For given function type `Fn`, and any type `A` (any in this context means we don't restrict the type, and I don't have in mind any type 😉) create a generic type which will take `Fn` as the first argument, `A` as the second, and will produce function type `G` which will be the same as `Fn` but with appended argument `A` as a last one.
+  For given function type `Fn`, and any type `A` (any in this context means we don't restrict the type,
+  and I don't have in mind any type 😉) create a generic type which will take `Fn` as the first argument,
+  `A` as the second, and will produce function type `G` which will be the same as `Fn` but with appended
+  argument `A` as a last one.
 
   For example,
 
@@ -23,7 +26,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends Function, A> = Fn extends (...args: infer Args) => infer Returned
+  ? (...args: [...Args, A]) => Returned
+  : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
