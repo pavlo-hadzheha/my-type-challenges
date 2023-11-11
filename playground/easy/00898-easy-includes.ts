3784953e81b4ae6d6479f65dd735a05e928e1e9b
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-11 18:40:50 +0200

    [softonix-assessment] [DONE] 00898-easy-includes.ts

--- playground/easy/00898-easy-includes.ts
+++ playground/easy/00898-easy-includes.ts
@@ -5,7 +5,8 @@
 
   ### Question
 
-  Implement the JavaScript `Array.includes` function in the type system. A type takes the two arguments. The output should be a boolean `true` or `false`.
+  Implement the JavaScript `Array.includes` function in the type system. A type takes the two arguments.
+  The output should be a boolean `true` or `false`.
 
   For example:
 
@@ -18,7 +19,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends readonly unknown[], U> =
+    T extends [infer First, ...infer Rest]
+      ? Equal<First, U> extends true ? true : Includes<Rest, U>
+      : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
