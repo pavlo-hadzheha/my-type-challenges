9253784dde75300fc15f4340137eee36f0848142
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-12 13:31:00 +0200

    [softonix-assessment] [DONE] 01097-medium-isunion.ts

--- playground/medium/01097-medium-isunion.ts
+++ playground/medium/01097-medium-isunion.ts
@@ -20,7 +20,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type IsUnion<T> = any
+type IsUnionImpl<T, C extends T = T> = (T extends T
+  ? C extends T
+    ? true
+    : unknown
+  : never) extends true ? false : true
+type IsUnion<T> = IsUnionImpl<T>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
