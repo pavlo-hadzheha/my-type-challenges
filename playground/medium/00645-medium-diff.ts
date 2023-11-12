eac99863ae3578386c576b61584499b1c7d6a824
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-12 13:02:54 +0200

    [softonix-assessment] [DONE] 00645-medium-diff.ts

--- playground/medium/00645-medium-diff.ts
+++ playground/medium/00645-medium-diff.ts
@@ -12,7 +12,13 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Diff<O, O1> = any
+type Diff<T, U> = {
+  [K in Exclude<keyof T, keyof U> | Exclude<keyof U, keyof T>]: K extends keyof T
+    ? T[K]
+    : K extends keyof U
+      ? U[K]
+      : never
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
