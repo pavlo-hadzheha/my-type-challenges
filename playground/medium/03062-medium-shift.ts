58214271e30bf8859c5f6e9fe9dc5d217e0f5717
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-12 17:07:27 +0200

    [softonix-assessment] [DONE] 03062-medium-shift.ts

--- playground/medium/03062-medium-shift.ts
+++ playground/medium/03062-medium-shift.ts
@@ -18,7 +18,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Shift<T> = any
+type Shift<T extends any[]> = T extends [infer _, ...infer R] ? R : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
