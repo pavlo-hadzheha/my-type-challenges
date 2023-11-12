fcab38053c77dc229f8bc566a42b46651a5e394b
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-12 13:33:42 +0200

    [softonix-assessment] [DONE] 01130-medium-replacekeys.ts

--- playground/medium/01130-medium-replacekeys.ts
+++ playground/medium/01130-medium-replacekeys.ts
@@ -45,7 +45,13 @@
 
 /* _____________ Your Code Here _____________ */
 
-type ReplaceKeys<U, T, Y> = any
+type ReplaceKeys<U, T, Y> = {
+  [K in keyof U]: K extends T
+    ? K extends keyof Y
+      ? Y[K]
+      : never
+    : U[K]
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
