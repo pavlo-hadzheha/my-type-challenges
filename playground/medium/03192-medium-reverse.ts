e1cf29d7a64570ed5aa47581b9e9ca2365a5a3b5
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-12 17:20:19 +0200

    [softonix-assessment] [DONE] 03192-medium-reverse.ts

--- playground/medium/03192-medium-reverse.ts
+++ playground/medium/03192-medium-reverse.ts
@@ -19,7 +19,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Reverse<T> = any
+type Reverse<T extends any[]> = T extends [infer F, ...infer R]
+  ? [...Reverse<R>, F]
+  : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
