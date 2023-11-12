11d974d87be900884fac952b096489c743bc2752
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-12 17:30:31 +0200

    [softonix-assessment] [DONE] 03243-medium-flattendepth.ts

--- playground/medium/03243-medium-flattendepth.ts
+++ playground/medium/03243-medium-flattendepth.ts
@@ -21,8 +21,17 @@
 
 /* _____________ Your Code Here _____________ */
 
-type FlattenDepth = any
-
+type FlattenDepth<
+    T extends any[],
+    S extends number = 1,
+    U extends any[] = [],
+> = U['length'] extends S
+  ? T
+  : T extends [infer F, ...infer R]
+    ? F extends any[]
+      ? [...FlattenDepth<F, S, [...U, 1]>, ...FlattenDepth<R, S, U>]
+      : [F, ...FlattenDepth<R, S, U>]
+    : T
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
