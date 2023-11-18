80abcf2abea77e519ec9e8c65371ccd5e966fd70
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-18 20:28:57 +0200

    [softonix-assessment] [DONE] 04471-medium-zip.ts

--- playground/medium/04471-medium-zip.ts
+++ playground/medium/04471-medium-zip.ts
@@ -15,7 +15,13 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Zip<T, U> = any
+type Zip<
+    T extends any[],
+    U extends any[],
+    Result extends any[][] = [],
+> = Result['length'] extends T['length'] | U['length']
+  ? Result
+  : Zip<T, U, [...Result, [T[Result['length']], U[Result['length']]]]>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -25,6 +31,7 @@ type cases = [
   Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
   Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
   Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[1, 2, 3], []>, []>>,
   Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
 ]
 
