ad44d494c07e2b1e2faadf0ebc7161198a905232
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-19 06:56:10 +0200

    [softonix-assessment] [DONE] 09142-medium-checkrepeatedchars.ts

--- playground/medium/09142-medium-checkrepeatedchars.ts
+++ playground/medium/09142-medium-checkrepeatedchars.ts
@@ -19,15 +19,26 @@
 
 /* _____________ Your Code Here _____________ */
 
-type CheckRepeatedChars<T extends string> = any
+// type CheckRepeatedChars<T extends string, U = never> =
+//       T extends `${infer F}${infer R}`
+//         ? F extends U
+//           ? true
+//           : CheckRepeatedChars<R, U | F>
+//         : false
+
+type CheckRepeatedChars<T extends string> =
+    T extends `${infer F}${infer R}`
+      ? R extends `${string}${F}${string}`
+        ? true
+        : CheckRepeatedChars<R>
+      : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
-import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<CheckRepeatedChars<'abc'>, false>>,
-  Expect<Equal<CheckRepeatedChars<'abb'>, true>>,
+  Expect<Equal<CheckRepeatedChars<'acbbc'>, true>>,
   Expect<Equal<CheckRepeatedChars<'cbc'>, true>>,
   Expect<Equal<CheckRepeatedChars<''>, false>>,
 ]
