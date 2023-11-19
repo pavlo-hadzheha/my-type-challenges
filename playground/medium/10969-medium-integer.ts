905a6dffa1bfd4444bb7052d5cb4e9191b2cd121
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-20 01:45:52 +0200

    [softonix-assessment] [DONE] 010969-medium-integer.ts

--- playground/medium/10969-medium-integer.ts
+++ playground/medium/10969-medium-integer.ts
@@ -12,18 +12,23 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Integer<T> = any
+type Integer<N extends number> = number extends N
+  ? never
+  : `${N}` extends `${string}.${infer End}`
+    ? End extends `${string}${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}${string}` ? never : N
+    : N
+
+type IntegerShort<T> = `${T & number}` extends `${bigint}` ? T : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
-import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 let x = 1
 let y = 1 as const
 
 type cases1 = [
   Expect<Equal<Integer<1>, 1>>,
-  Expect<Equal<Integer<1.1>, never>>,
+  Expect<Equal<IntegerShort<1.1>, never>>,
   Expect<Equal<Integer<1.0>, 1>>,
   Expect<Equal<Integer<1.000000000>, 1>>,
   Expect<Equal<Integer<0.5>, never>>,
@@ -31,6 +36,18 @@ type cases1 = [
   Expect<Equal<Integer<28.101>, never>>,
   Expect<Equal<Integer<typeof x>, never>>,
   Expect<Equal<Integer<typeof y>, 1>>,
+]
+
+type cases2 = [
+  Expect<Equal<IntegerShort<1>, 1>>,
+  Expect<Equal<IntegerShort<1.1>, never>>,
+  Expect<Equal<IntegerShort<1.0>, 1>>,
+  Expect<Equal<IntegerShort<1.000000000>, 1>>,
+  Expect<Equal<IntegerShort<0.5>, never>>,
+  Expect<Equal<IntegerShort<28.00>, 28>>,
+  Expect<Equal<IntegerShort<28.101>, never>>,
+  Expect<Equal<IntegerShort<typeof x>, never>>,
+  Expect<Equal<IntegerShort<typeof y>, 1>>,
 ]
 
 /* _____________ Further Steps _____________ */
