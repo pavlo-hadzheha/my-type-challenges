f5faa830b418884e9bb760ff7162af7b05b1c6fb
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-19 06:37:29 +0200

    [softonix-assessment] [DONE] 08987-medium-subsequence.ts

--- playground/medium/08987-medium-subsequence.ts
+++ playground/medium/08987-medium-subsequence.ts
@@ -20,7 +20,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Subsequence<T extends any[]> = any
+type Subsequence<T extends any[]> = T extends [infer F, ...infer Rest]
+  ? Subsequence<Rest> | [F, ...Subsequence<Rest>]
+  : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
