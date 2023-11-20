d86c70727b4cb1d5f348afde67a38c9f067e4867
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-20 04:26:08 +0200

    [softonix-assessment] [DONE] 26401-medium-json-schema-to-ts.ts

--- playground/medium/26401-medium-json-schema-to-typescript.ts
+++ playground/medium/26401-medium-json-schema-to-typescript.ts
@@ -17,7 +17,50 @@
 
 /* _____________ Your Code Here _____________ */
 
-type JSONSchema2TS<T> = any
+type Primitives = {
+  string: string
+  number: number
+  boolean: boolean
+}
+
+type HandlePrimitives<T, Type extends keyof Primitives> = T extends {
+  enum: unknown[]
+}
+  ? T['enum'][number]
+  : Primitives[Type]
+
+type HandleObject<T> = T extends {
+  properties: infer Properties extends Record<string, unknown>
+}
+  ? T extends { required: infer Required extends unknown[] }
+    ? Omit<
+            {
+              [K in Required[number] & keyof Properties]: JSONSchema2TS<
+                Properties[K]
+            >;
+            } & {
+              [K in Exclude<keyof Properties, Required[number]>]?: JSONSchema2TS<
+              Properties[K]
+          >;
+            },
+            never
+        >
+    : {
+        [K in keyof Properties]?: JSONSchema2TS<Properties[K]>;
+      }
+  : Record<string, unknown>
+
+type HandleArray<T> = T extends { items: infer Items }
+  ? JSONSchema2TS<Items>[]
+  : unknown[]
+
+type JSONSchema2TS<T> = T extends { type: infer Type }
+  ? Type extends keyof Primitives
+    ? HandlePrimitives<T, Type>
+    : Type extends 'object'
+      ? HandleObject<T>
+      : HandleArray<T>
+  : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
