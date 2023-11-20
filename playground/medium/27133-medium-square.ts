a326bc06a564c0f806bf324d138f96083f102ee7
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2023-11-20 04:37:04 +0200

    [softonix-assessment] [DONE] 27133-medium-square.ts

--- playground/medium/27133-medium-square.ts
+++ playground/medium/27133-medium-square.ts
@@ -12,7 +12,20 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Square<N extends number> = number
+type MakeCounter<T extends number, _Result extends 1[] = []> = _Result['length'] extends T | 999 ? _Result : MakeCounter<T, [..._Result, 1]>
+/** 扁平化 */
+type FlatCounterCounter<T extends any[][], _Result extends any[] = []> = T extends [infer F extends any[], ...infer R extends any[][]] ?
+  FlatCounterCounter<R, [..._Result, ...F]> :
+  _Result
+type SimpleMultiply<T extends number, U extends number, _CounterCounter extends 1[][] = []> = T | U extends 100 ? 10000 : _CounterCounter['length'] extends U ?
+  FlatCounterCounter<_CounterCounter>['length'] :
+  SimpleMultiply<T, U, [..._CounterCounter, MakeCounter<T>]>
+
+/** 100以内 */
+type Square<N extends number> =
+    `${N}` extends `-${infer NN extends number}}`
+      ? SimpleMultiply<NN, NN>
+      : SimpleMultiply<N, N>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -23,6 +36,7 @@ type cases = [
   Expect<Equal<Square<3>, 9>>,
   Expect<Equal<Square<20>, 400>>,
   Expect<Equal<Square<100>, 10000>>,
+  Expect<Equal<Square<99>, 9801>>,
 
   // Negative numbers
   Expect<Equal<Square<-2>, 4>>,
